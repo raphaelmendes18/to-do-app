ecd3267923ab7c22979979cf0a56241db629882c
Raphael Mendes <raphaelmendes18@gmail.com>
2025-05-12 21:07:38 -0300

    localStorage support

--- src/app/todo-data.service.ts
+++ src/app/todo-data.service.ts
@@ -3,36 +3,57 @@ import { BehaviorSubject } from 'rxjs';
 import { Task } from './types/task';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoDataService {
-
   private todos: Task[] = [];
   private todoDataSource = new BehaviorSubject<Task[]>([]);
   todoData$ = this.todoDataSource.asObservable();
 
-  updateTodoData(data: Task) {
-    this.todos.push(data);
+  constructor() {
+    this.todos = this.loadFromLocalStorage();
     this.todoDataSource.next([...this.todos]);
   }
 
-  deleteTodoItem(index: number){
+  updateTodoData(data: Task) {
+    this.todos.push(data);
+    this.saveToLocalStorage(this.todos);
+    this.todoDataSource.next([...this.todos]);
+  }
+
+  deleteTodoItem(index: number) {
     this.todos.splice(index, 1);
     this.todoDataSource.next([...this.todos]);
+    this.saveToLocalStorage(this.todos);
   }
 
   toggleComplete = (index: number) => {
     this.todos[index].completed = !this.todos[index].completed;
     this.todoDataSource.next([...this.todos]);
-  }
+    this.saveToLocalStorage(this.todos);
+  };
 
   getTodoItem = (index: number) => {
     return this.todos[index];
-  }
-  
+  };
+
   editTodoItem = (index: number, task: Task) => {
     this.todos[index] = task;
     this.todoDataSource.next([...this.todos]);
+    this.saveToLocalStorage(this.todos);
+  };
+
+  private loadFromLocalStorage(): Task[] {
+    let todos: Task[] = [];
+    if (localStorage.getItem('todos')) {
+      todos = JSON.parse(localStorage.getItem('todos')!) as Task[];
+    } else {
+      localStorage.setItem('todos', JSON.stringify([]));
+    }
+    return todos;
   }
 
+  private saveToLocalStorage(todos: Task[]) {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }
 }
